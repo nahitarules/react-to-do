e2ba17a8405e9b2ed797aca45175a7ae3fe5d94c
nahome <nahom.teshome@yahoo.com>
2019-01-19 02:22:30 -0800

    react-to-do events assignment complete

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -4,8 +4,10 @@ class ToDo extends Component {
   render() {
      return (
        <li>
+
          <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
          <span>{ this.props.description }</span>
+         <button onClick={ this.props.deleteTodo }>Delete</button>
        </li>
      );
    }
